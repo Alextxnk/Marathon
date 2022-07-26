400ea604e9b01fa7e020872b0196da82fc40b22c
Alextxnk <alexsolov766@gmail.com>
2022-07-26 21:01:56 +0300

    Day 03

---
+++ day-03-slider/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+
+const upBtn = document.querySelector('.up-button');
+const downBtn = document.querySelector('.down-button');
+const sidebar = document.querySelector('.sidebar');
+const container = document.querySelector('.container');
+const mainSlide = document.querySelector('.main-slide');
+
+const slidesCount = mainSlide.querySelectorAll('div').length; // считаем количество слайдов 
+
+let activeSlideIndex = 0;
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+
+function changeSlide(direction) {
+   if(direction === 'up') {
+      activeSlideIndex++;
+      if(activeSlideIndex === slidesCount) {
+         activeSlideIndex = 0; // если вышли за рамки - обнуляем индекс 
+      }
+   } else if(direction === 'down') {
+      activeSlideIndex--;
+      if(activeSlideIndex < 0) {
+         activeSlideIndex = slidesCount - 1;
+      }
+   }
+
+   const height = container.clientHeight; // получаем высоту слайда 
+
+   mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+
+   sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
+}
+
+
+upBtn.addEventListener('click', () => {
+   changeSlide('up');
+});
+
+downBtn.addEventListener('click', () => {
+   changeSlide('down');
+});
+
