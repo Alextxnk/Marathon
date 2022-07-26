4f69542d66ed0d9762b22cf70f865c18285a6a96
Alextxnk <alexsolov766@gmail.com>
2022-07-26 12:15:02 +0300

    Day 01

--- day-01-cards/app.js
+++ day-01-cards/app.js
@@ -1,20 +1,27 @@
 'use strict';
 
-const slides = document.querySelectorAll('.slide');
 
-// теперь нужно пройти циклом по каждому из этих слайдов и добавить к нему определенного слушателя  
-// два способа, как можно проходить по подобным массивам в JavaScript for...of и forEach
+function slidesPlugin(activeSlide = 2) {
+   const slides = document.querySelectorAll('.slide');
 
-for (const slide of slides) {
-   slide.addEventListener('click', () => {
-      clearActiveClasses();
+   // теперь нужно пройти циклом по каждому из этих слайдов и добавить к нему определенного слушателя  
+   // два способа, как можно проходить по подобным массивам в JavaScript for...of и forEach
 
-      slide.classList.add('active');
-   }); // добавляем слушателя событий 
+   slides[activeSlide].classList.add('active');
+
+   for (const slide of slides) {
+      slide.addEventListener('click', () => {
+         clearActiveClasses();
+
+         slide.classList.add('active');
+      }); // добавляем слушателя событий 
+   }
+
+   function clearActiveClasses() {
+      slides.forEach((slide) => {
+         slide.classList.remove('active');
+      });
+   }
 }
 
-function clearActiveClasses() {
-   slides.forEach((slide) => {
-      slide.classList.remove('active');
-   });
-}
+slidesPlugin(3); 
